e3625252f23f36a037b013f5c8d366f61354d202
Lastepeck <wilgnerkatz@gmail.com>
2022-07-27 00:43:03 -0700

    Adding Bootstrap grid to index page

--- views/error404.jsx
+++ views/error404.jsx
@@ -7,6 +7,14 @@ function error404 () {
           <main>
               <h1>404: PAGE NOT FOUND</h1>
               <p>Oops, sorry, we can't find this page!</p>
+              <div>
+                    <img src="/images/cool-cat.jpg" alt="cat with sun glasses" />
+                    <div>
+                    Photo by <a href="https://unsplash.com/@raouldroog?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Raoul Droog</a> on <a href="https://unsplash.com/s/photos/cat?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+  
+                    </div>
+                 </div>
+                
           </main>
       </Def>
     )
